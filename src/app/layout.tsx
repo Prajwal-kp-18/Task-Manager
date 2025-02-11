ffe6d38e05dd7728ea47a3b36238ef3116c2bfec
Prajwal-kp-18 <prajwal.kp.1817@gmail.com>
2025-02-11 20:02:01 +0530

    completed the database and auth setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import "@/app/globals.css";
+import type { Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+
+import { Archivo } from "next/font/google";
+import { Libre_Franklin } from "next/font/google";
+import { Providers } from "./providers";
+import { Toaster } from "@/components/ui/toaster";
+import { Header } from "./_header/header";
+
+const archivo = Archivo({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-archivo",
+});
+const libre_franklin = Libre_Franklin({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-libre_franklin",
+});
+
+export const metadata: Metadata = {
+  title: "Task Manager",
+  description:
+    "A simple and efficient task management application to organize, track, and prioritize your tasks seamlessly.",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background antialiased",
+          archivo.variable + " " + libre_franklin.variable,
+        )}
+      >
+        <Providers>
+          <NextTopLoader />
+          <Header />
+          <div className="container mx-auto w-full py-12">{children}</div>
+        </Providers>
+        <Toaster />
+      </body>
+    </html>
+  );
+}
